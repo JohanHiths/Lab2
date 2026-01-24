23abbd1b81dd014cbf50a00a69d14082a6067c46
JohanHiths <Johan.Hietala@iths.se>
2026-01-24 19:22:57 +0100

    Börjat med PaymenProcessor

---
+++ src/main/java/com/example/payment/EmailService.java
@@ -0,0 +1,15 @@
+package com.example.payment;
+
+import com.example.Booking;
+
+import java.sql.PreparedStatement;
+
+public interface EmailService {
+
+
+
+    void sendPaymentConfirmation(String email, double amount);
+    void DatabaseConnection();
+
+    PreparedStatement getInstance();
+}
