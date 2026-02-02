fc766cdd047948303030896c39f730320a0aff1d
JohanHiths <Johan.Hietala@iths.se>
2026-02-02 21:47:41 +0100

    Mer kod - void add och getName så addItem ska vara grön

--- src/main/java/com/example/shopping/Item.java
+++ src/main/java/com/example/shopping/Item.java
@@ -1,4 +1,13 @@
 package com.example.shopping;
 
 public class Item {
+    String name;
+
+    public void add(String name) {
+        this.name = name;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
 }
--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,15 +1,25 @@
 package com.example;
 
 import com.example.shopping.Item;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class ShoppingCartTest {
 
 
     @Test
+    @DisplayName("Ska kunna lägga till ett item i vagnen")
     void addItem() {
         Item item = new Item();
 
+        item.add("Choklad");
+
+
+        assertThat(item.getName("Choklad")).isEqualTo("Choklad");
+
     }
+
 }
