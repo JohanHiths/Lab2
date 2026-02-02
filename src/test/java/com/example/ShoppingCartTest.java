96e251559aad7171fe19d16e0957ce0a8fa46b0c
JohanHiths <Johan.Hietala@iths.se>
2026-02-02 21:22:01 +0100

    Börjar med Uppgift 2, + ShoppingCartTest och Item

---
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.example.shopping.Item;
+import org.junit.jupiter.api.Test;
+
+
+public class ShoppingCartTest {
+
+
+    @Test
+    void addItem() {
+        Item item = new Item();
+
+    }
+}
